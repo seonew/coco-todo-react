bc737100fb813ab5b28304025b582036c806b327
seonew <seonew.888@gmail.com>
2019-08-21 23:32:24 +0900

    Update TODO API

--- src/api/index.js
+++ src/api/index.js
@@ -1,60 +1,89 @@
-import axios from 'axios';
+import axios from "axios";
 
-const BASE_URL = 'https://us-central1-todo-restful.cloudfunctions.net/'
+const BASE_URL = "https://us-central1-todo-restful.cloudfunctions.net/";
 
 export default {
-  changeTodoState: function(data) {
-    axios.defaults.headers.common['Authorization'] = 'Bearer ' + data.authToken;
-    axios.defaults.headers.post['Content-Type'] = 'application/json';
+  changeCheckItemState: function(data) {
+    axios.defaults.headers.common["Authorization"] = "Bearer " + data.authToken;
+    axios.defaults.headers.post["Content-Type"] = "application/json";
 
-    return axios.patch(BASE_URL + 'todos/' + data.id, data)
-    .then((response) => {
-      console.log(response)
-      return response
-    })
+    return axios
+      .patch(BASE_URL + "todos/" + data.todoId + "/checklist/" + data.id, data)
+      .then(response => {
+        console.log(response);
+        return response;
+      });
   },
-  delete: function (data) {
-    axios.defaults.headers.common['Authorization'] = 'Bearer ' + data.authToken;
-    axios.defaults.headers.post['Content-Type'] = 'application/json';
+  checkItemDelete: function(data) {
+    axios.defaults.headers.common["Authorization"] = "Bearer " + data.authToken;
+    axios.defaults.headers.post["Content-Type"] = "application/json";
 
-    return axios.delete(BASE_URL + 'todos/' + data.id, data)
-    .then((response) => {
-      console.log(response)
-    })
+    return axios
+      .delete(BASE_URL + "todos/" + data.todoId + "/checklist/" + data.id, data)
+      .then(response => {
+        console.log(response);
+      });
   },
-  update: function (data) {
-    axios.defaults.headers.common['Authorization'] = 'Bearer ' + data.authToken;
-    axios.defaults.headers.post['Content-Type'] = 'application/json';
+  checkItemUpdate: function(data) {
+    axios.defaults.headers.common["Authorization"] = "Bearer " + data.authToken;
+    axios.defaults.headers.post["Content-Type"] = "application/json";
 
-    return axios.put(BASE_URL + 'todos/' + data.id, data)
-    .then((response) => {
-      console.log(response)
-    })
+    return axios
+      .put(BASE_URL + "todos/" + data.todoId + "/checklist/" + data.id, data)
+      .then(response => {
+        console.log(response);
+      });
   },
-  insert: function (data) {
-    axios.defaults.headers.common['Authorization'] = 'Bearer ' + data.authToken;
-    axios.defaults.headers.post['Content-Type'] = 'application/json';
+  checkItemInsert: function(data) {
+    axios.defaults.headers.common["Authorization"] = "Bearer " + data.authToken;
+    axios.defaults.headers.post["Content-Type"] = "application/json";
 
-    return axios.post(BASE_URL + 'todos', data)
-    .then((response) => {
-      console.log(response)
-      return response
-    })
+    return axios
+      .post(BASE_URL + "todos/" + data.todoId + "/checklist", data)
+      .then(response => {
+        console.log(response);
+        return response;
+      });
+  },
+  delete: function(data) {
+    axios.defaults.headers.common["Authorization"] = "Bearer " + data.authToken;
+    axios.defaults.headers.post["Content-Type"] = "application/json";
+
+    return axios.delete(BASE_URL + "todos/" + data.id, data).then(response => {
+      console.log(response);
+    });
+  },
+  update: function(data) {
+    axios.defaults.headers.common["Authorization"] = "Bearer " + data.authToken;
+    axios.defaults.headers.post["Content-Type"] = "application/json";
+
+    return axios.put(BASE_URL + "todos/" + data.id, data).then(response => {
+      console.log(response);
+    });
+  },
+  insert: function(data) {
+    axios.defaults.headers.common["Authorization"] = "Bearer " + data.authToken;
+    axios.defaults.headers.post["Content-Type"] = "application/json";
+
+    return axios.post(BASE_URL + "todos", data).then(response => {
+      console.log(response);
+      return response;
+    });
   },
   select: function(authToken) {
-    axios.defaults.headers.common['Authorization'] = 'Bearer ' + authToken;
-    axios.defaults.headers.post['Content-Type'] = 'application/json';
+    axios.defaults.headers.common["Authorization"] = "Bearer " + authToken;
+    axios.defaults.headers.post["Content-Type"] = "application/json";
 
-    return axios.get(BASE_URL + 'todos')
-      .then((response) => {
-        return response
-    })
+    return axios.get(BASE_URL + "todos").then(response => {
+      return response;
+    });
   },
-  authorize: function (userId, userPassword) {
-    return axios.post(BASE_URL + 'authorize', {"id" : userId, "password" : userPassword })
-      .then((response) => {
-        localStorage.setItem('authToken', response.data.token);
-        return response
-    })
+  authorize: function(userId, userPassword) {
+    return axios
+      .post(BASE_URL + "authorize", { id: userId, password: userPassword })
+      .then(response => {
+        localStorage.setItem("authToken", response.data.token);
+        return response;
+      });
   }
 };
